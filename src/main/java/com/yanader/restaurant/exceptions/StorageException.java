0cfa87ce3518822f36f01be601b65a4803ee2ffc
Ste <mrbarrow@hotmail.co.uk>
2025-07-15 16:24:31 +0100

    feat:storageException

---
+++ src/main/java/com/yanader/restaurant/exceptions/StorageException.java
@@ -0,0 +1,19 @@
+package com.yanader.restaurant.exceptions;
+
+public class StorageException extends BaseException{
+
+    public StorageException() {
+    }
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StorageException(Throwable cause) {
+        super(cause);
+    }
+}
