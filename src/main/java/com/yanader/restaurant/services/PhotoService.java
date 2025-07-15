b5e3f6aaac6acf8f322cf21de558f70cf29535bd
Ste <mrbarrow@hotmail.co.uk>
2025-07-15 16:54:47 +0100

    feat:PhotoService

---
+++ src/main/java/com/yanader/restaurant/services/PhotoService.java
@@ -0,0 +1,12 @@
+package com.yanader.restaurant.services;
+
+import com.yanader.restaurant.domain.entities.Photo;
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+public interface PhotoService {
+    Photo uploadPhoto(MultipartFile file);
+    Optional<Resource> getPhotoAsResource(String id);
+}
