44e767f4c684bb0bf1c44da23d49803a8d5ef7ec
Ste <mrbarrow@hotmail.co.uk>
2025-07-15 22:45:42 +0100

    feat:PhotoDTO

---
+++ src/main/java/com/yanader/restaurant/domain/dtos/PhotoDTO.java
@@ -0,0 +1,17 @@
+package com.yanader.restaurant.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PhotoDTO {
+    private String url;
+    private LocalDateTime uploadDate;
+}
