4f4c58692f3b3a988c7e148d7688faceceeae5ef
Ste <mrbarrow@hotmail.co.uk>
2025-07-15 16:20:16 +0100

    feat:StorageService

---
+++ src/main/java/com/yanader/restaurant/services/StorageService.java
@@ -0,0 +1,12 @@
+package com.yanader.restaurant.services;
+
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+public interface StorageService {
+    String store(MultipartFile file, String filename);
+    Optional<Resource> loadAsResource(String id);
+}
