8dee233b41196a362467e058e3d2d5839a9c8119
Ste <mrbarrow@hotmail.co.uk>
2025-07-22 19:54:17 +0100

    feat:ErrorController

---
+++ src/main/java/com/yanader/restaurant/controllers/ErrorController.java
@@ -0,0 +1,55 @@
+package com.yanader.restaurant.controllers;
+
+import com.yanader.restaurant.domain.dtos.ErrorDto;
+import com.yanader.restaurant.exceptions.BaseException;
+import com.yanader.restaurant.exceptions.StorageException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
+@Slf4j
+public class ErrorController {
+
+    @ExceptionHandler(StorageException.class)
+    public ResponseEntity<ErrorDto> handleStorageException(StorageException ex) {
+        log.error("Caught StorageException", ex);
+
+        ErrorDto errorDto = ErrorDto.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("Unable to save or retrieve resources at this time")
+                .build();
+
+        return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ErrorDto> handleBaseException(BaseException ex) {
+        log.error("Caught baseException", ex);
+
+        ErrorDto errorDto = ErrorDto.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An unexpected error occurred")
+                .build();
+
+        return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDto> handleException(BaseException ex) {
+        log.error("Caught unexpected exception", ex);
+
+        ErrorDto errorDto = ErrorDto.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An unexpected error occurred")
+                .build();
+
+        return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
