7c1cafc58017d1a1d4ca053eaac9ce2da50dcee1
Ste <mrbarrow@hotmail.co.uk>
2025-07-23 21:13:27 +0100

    feat:GeoLocation

---
+++ src/main/java/com/yanader/restaurant/domain/GeoLocation.java
@@ -0,0 +1,16 @@
+package com.yanader.restaurant.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GeoLocation {
+
+    private Double latitude;
+    private Double longitude;
+}
