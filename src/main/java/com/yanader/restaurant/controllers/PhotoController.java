59347c424219cc2567117898c7a3db98f87d2981
Ste <mrbarrow@hotmail.co.uk>
2025-07-23 20:48:24 +0100

    feat:PhotoController.getPhoto()

--- src/main/java/com/yanader/restaurant/controllers/PhotoController.java
+++ src/main/java/com/yanader/restaurant/controllers/PhotoController.java
@@ -5,6 +5,11 @@ import com.yanader.restaurant.domain.entities.Photo;
 import com.yanader.restaurant.mappers.PhotoMapper;
 import com.yanader.restaurant.services.PhotoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,6 +25,19 @@ public class PhotoController {
     public PhotoDto uploadPhoto(@RequestParam("file")MultipartFile file) {
         Photo savedPhoto = photoService.uploadPhoto(file);
         return photoMapper.toDto(savedPhoto);
+    }
+
+    @GetMapping(path = "/{id:.+}")
+    public ResponseEntity<Resource> getPhoto(@PathVariable String id) {
+        return photoService.getPhotoAsResource(id).map(photo ->
+                ResponseEntity.ok()
+                        .contentType(
+                                MediaTypeFactory.getMediaType(photo)
+                                        .orElse(MediaType.APPLICATION_OCTET_STREAM)
+                        )
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "inline")
+                        .body(photo)
+        ).orElse(ResponseEntity.notFound().build());
     }
 
 }
