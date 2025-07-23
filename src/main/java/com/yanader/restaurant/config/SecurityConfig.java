3752383a648d1609db460f99a58174eb9268d519
Ste <mrbarrow@hotmail.co.uk>
2025-07-23 20:50:49 +0100

    update:securityconfig

--- src/main/java/com/yanader/restaurant/config/SecurityConfig.java
+++ src/main/java/com/yanader/restaurant/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.yanader.restaurant.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,7 +17,9 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth ->
-                        auth.anyRequest().authenticated()
+                        auth
+                                .requestMatchers(HttpMethod.GET, "/api/photos/**").permitAll()
+                                .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwt ->
