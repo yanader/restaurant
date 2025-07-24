59c543772cc44b62eb36a823b132ebeb47d5b7b5
Ste <mrbarrow@hotmail.co.uk>
2025-07-24 21:13:39 +0100

    feat:RestaurantController

---
+++ src/main/java/com/yanader/restaurant/controllers/RestaurantController.java
@@ -0,0 +1,36 @@
+package com.yanader.restaurant.controllers;
+
+import com.yanader.restaurant.domain.RestaurantCreateUpdateRequest;
+import com.yanader.restaurant.domain.dtos.RestaurantCreateUpdateRequestDto;
+import com.yanader.restaurant.domain.dtos.RestaurantDto;
+import com.yanader.restaurant.domain.entities.Restaurant;
+import com.yanader.restaurant.mappers.RestaurantMapper;
+import com.yanader.restaurant.services.RestaurantService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/restaurants")
+@RequiredArgsConstructor
+public class RestaurantController {
+
+    private final RestaurantService restaurantService;
+    private final RestaurantMapper restaurantMapper;
+
+    @PostMapping
+    public ResponseEntity<RestaurantDto> createRestaurant(
+            @Valid @RequestBody RestaurantCreateUpdateRequestDto request
+    ) {
+        RestaurantCreateUpdateRequest restaurantCreateUpdateRequest = restaurantMapper
+                .toRestaurantCreateUpdateRequest(request);
+
+        Restaurant restaurant = restaurantService.createRestaurant(restaurantCreateUpdateRequest);
+        RestaurantDto createdRestaurantDto = restaurantMapper.toRestaurantDto(restaurant);
+        return ResponseEntity.ok(createdRestaurantDto);
+    }
+}
