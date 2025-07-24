33a8182f0ea7594fe517401e7cd5d6c715bf9ca9
Ste <mrbarrow@hotmail.co.uk>
2025-07-24 21:33:07 +0100

    feat:ErrorController.handleMethodArgumentNotValidException

--- src/main/java/com/yanader/restaurant/controllers/ErrorController.java
+++ src/main/java/com/yanader/restaurant/controllers/ErrorController.java
@@ -4,17 +4,41 @@ import com.yanader.restaurant.domain.dtos.ErrorDto;
 import com.yanader.restaurant.exceptions.BaseException;
 import com.yanader.restaurant.exceptions.StorageException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Method;
+import java.util.stream.Collectors;
+
 @RestController
 @ControllerAdvice
 @Slf4j
 public class ErrorController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.error("Caught MethodArgumentNotValidException", ex);
+
+        String errorMessage = ex
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField()  + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+         ErrorDto errorDto = ErrorDto.builder()
+                 .status(HttpStatus.BAD_REQUEST.value())
+                 .message(errorMessage)
+                 .build();
+
+         return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(StorageException.class)
     public ResponseEntity<ErrorDto> handleStorageException(StorageException ex) {
