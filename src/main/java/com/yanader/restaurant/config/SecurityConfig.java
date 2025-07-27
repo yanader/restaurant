0631d1cf582d4eb69ff61d8fcff2cc3731efee52
Ste <mrbarrow@hotmail.co.uk>
2025-07-27 11:52:45 +0100

    update:securityconfig

--- src/main/java/com/yanader/restaurant/config/SecurityConfig.java
+++ src/main/java/com/yanader/restaurant/config/SecurityConfig.java
@@ -19,6 +19,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth ->
                         auth
                                 .requestMatchers(HttpMethod.GET, "/api/photos/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/restaurants/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 ->
