670d7a78498eba8224e575a2a35630a8aef8a7e7
Ste <mrbarrow@hotmail.co.uk>
2025-07-27 11:43:26 +0100

    feat:RestaurantMapper update

--- src/main/java/com/yanader/restaurant/mappers/RestaurantMapper.java
+++ src/main/java/com/yanader/restaurant/mappers/RestaurantMapper.java
@@ -4,11 +4,16 @@ import com.yanader.restaurant.domain.RestaurantCreateUpdateRequest;
 import com.yanader.restaurant.domain.dtos.GeoPointDto;
 import com.yanader.restaurant.domain.dtos.RestaurantCreateUpdateRequestDto;
 import com.yanader.restaurant.domain.dtos.RestaurantDto;
+import com.yanader.restaurant.domain.dtos.RestaurantSummaryDto;
 import com.yanader.restaurant.domain.entities.Restaurant;
+import com.yanader.restaurant.domain.entities.Review;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.data.elasticsearch.core.geo.GeoPoint;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RestaurantMapper {
@@ -17,7 +22,17 @@ public interface RestaurantMapper {
 
     RestaurantDto toRestaurantDto(Restaurant restaurant);
 
+    @Mapping(source = "reviews", target = "totalReviews", qualifiedByName = "populateTotalReviews")
+    RestaurantSummaryDto toSummaryDto (Restaurant restaurant);
+
+    @Named("populateTotalReviews")
+    private Integer populateTotalReviews(List<Review> reviews) {
+        return reviews.size();
+    }
+
     @Mapping(target = "latitude", expression = "java(geoPoint.getLat())")
     @Mapping(target = "longitude", expression = "java(geoPoint.getLon())")
     GeoPointDto toGeoPointDto(GeoPoint geoPoint);
+
+
 }
