e6e0687ea25b60419c7cdfe08ecc61c365c9c29c
Ste <mrbarrow@hotmail.co.uk>
2025-07-27 11:27:55 +0100

    feat:RestaurantRepositoryQueries

--- src/main/java/com/yanader/restaurant/repositories/RestaurantRepository.java
+++ src/main/java/com/yanader/restaurant/repositories/RestaurantRepository.java
@@ -1,10 +1,51 @@
 package com.yanader.restaurant.repositories;
 
 import com.yanader.restaurant.domain.entities.Restaurant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
+
 @Repository
 public interface RestaurantRepository extends ElasticsearchRepository<Restaurant, String> {
-    // TODO: Custom queries
+
+    Page<Restaurant> findByAverageRatingGreaterThanEqual(Float minRating, Pageable pageable);
+
+    @Query("{" +
+            "  \"bool\": {" +
+            "    \"must\": [" +
+            "      {\"range\": {\"averageRating\": {\"gte\": ?1}}}" +
+            "    ]," +
+            "    \"should\": [" +
+            "      {\"fuzzy\": {\"name\": {\"value\": \"?0\", \"fuzziness\": \"AUTO\"}}}," +
+            "      {\"fuzzy\": {\"cuisineType\": {\"value\": \"?0\", \"fuzziness\": \"AUTO\"}}}" +
+            "    ]," +
+            "    \"minimum_should_match\": 1" +
+            "  }" +
+            "}" +
+            "}")
+    Page<Restaurant> findByQueryAndMinRating(String query, Float minRating, Pageable pageable);
+
+
+    @Query("{" +
+            "  \"bool\": {" +
+            "    \"must\": [" +
+            "      {\"geo_distance\": {" +
+            "        \"distance\": \"?2km\"," +
+            "        \"geoLocation\": {" +
+            "          \"lat\": ?0," +
+            "          \"lon\": ?1" +
+            "        }" +
+            "      }}" +
+            "    ]" +
+            "  }" +
+            "}")
+    Page<Restaurant> findByLocationNear(
+            Float latitude,
+            Float longitude,
+            Float radiusKm,
+            Pageable pageable);
+
 }
