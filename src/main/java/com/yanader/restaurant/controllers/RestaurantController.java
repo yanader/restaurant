ef2ed8f32622e35a58e5c03a52199844041a3cda
Ste <mrbarrow@hotmail.co.uk>
2025-07-28 22:08:26 +0100

    feat:getRestaurant

--- src/main/java/com/yanader/restaurant/controllers/RestaurantController.java
+++ src/main/java/com/yanader/restaurant/controllers/RestaurantController.java
@@ -54,4 +54,12 @@ public class RestaurantController {
 
         return searchResults.map(restaurantMapper::toSummaryDto);
     }
+
+    @GetMapping(path = "/{restaurant_id}")
+    public ResponseEntity<RestaurantDto> getRestaurant(@PathVariable("restaurant_id") String restaurantId) {
+        return restaurantService.getRestaurant(restaurantId)
+                .map(restaurant -> ResponseEntity.ok(restaurantMapper.toRestaurantDto(restaurant)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 }
